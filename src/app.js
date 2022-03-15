308c58f80a7abc4caa959c382ccccc61885f4fcb
LucasvdVegt <lpsvandervegt@gmail.com>
2022-03-15 15:14:17 +0100

    single prediction

--- src/app.js
+++ src/app.js
@@ -6,8 +6,8 @@ const trainingLabel = "class"
 const ignoredColumns = []
 
 let data      = loadData()
-let trainData = data.slice(0, Math.floor(data.length * 0.8))
-let testData  = data.slice(Math.floor(data.length * 0.8) + 1)
+// let trainData = data.slice(0, Math.floor(data.length * 0.8))
+// let testData  = data.slice(Math.floor(data.length * 0.8) + 1)
 
 
 function loadData() {
@@ -28,6 +28,63 @@ function trainModel(data){
     })
 
     let visual = new VegaTree('#view', 1200, 500, decisionTree.toJSON())
+
+    predict(decisionTree)
 }
 
 loadData()
+
+
+
+
+
+function predict(decisionTree){
+    let prediction = decisionTree.predict({
+        "cap-shape":                    "x",
+        "cap-surface":                  "s",
+        "cap-color":                    "n",
+        "bruises":                      "t",
+        "odor":                         "y",
+        "gill-attachment":              "f",
+        "gill-spacing":                 "c",
+        "gill-size":                    "n",
+        "gill-color":                   "k",
+        "stalk-shape":                  "e",
+        "stalk-root":                   "e",
+        "stalk-surface-above-ring":     "s",
+        "stalk-surface-below-ring":     "s",
+        "stalk-color-above-ring":       "w",
+        "stalk-color-below-ring":       "w",
+        "veil-type":                    "p",
+        "veil-color":                   "w",
+        "ring-number":                  "o",
+        "ring-type":                    "p",
+        "spore-print-color":            "k",
+        "population":                   "s",
+        "habita":                       "d"
+    })
+    
+    console.log(`this mushroom is ${prediction}`)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
